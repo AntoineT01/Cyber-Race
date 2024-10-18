3f6c31dfd872217a098a0b837d43aa3b58eed693
AntoineT01 <antoine.t5656@gmail.com>
2024-10-18 10:51:01 +0200

    feat(Area3D): Add floating movement to rotating piece

--- piece.gd
+++ piece.gd
@@ -3,14 +3,25 @@ extends Area3D
 signal piece_collected(by_player)
 
 @export var rotation_speed: float = 2.0
+@export var float_speed: float = 2.0  # Vitesse du mouvement de flottement
+@export var float_amplitude: float = 0.1  # Amplitude du mouvement de flottement
+
+var initial_y: float
+var time: float = 0.0
 
 func _ready():
 	# Rotation aléatoire initiale pour la variété
 	rotate_y(randf() * 2 * PI)
+	initial_y = global_transform.origin.y
 
 func _process(delta):
 	# Rotation continue
 	rotate_y(rotation_speed * delta)
+
+	# Mouvement de flottement
+	time += delta
+	var new_y = initial_y + sin(time * float_speed) * float_amplitude
+	global_transform.origin.y = new_y
 
 func _on_body_entered(body):
 	if body.is_in_group("players"):
