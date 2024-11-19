0386f4866d9332e312157a37939ef79bfef8f87e
Flintpop <tanguy.legoff2@gmail.com>
2024-11-19 15:37:13 +0100

    fix: Pieces max pas mises à jour entre les deux scenes en switch

--- main_scenes/piece_race.gd
+++ main_scenes/piece_race.gd
@@ -1,4 +1,4 @@
 extends Node
 
 func _ready() -> void:
-	ScoreManager.set_victory_condition("pieces", ScoreManager.victory_target)
+	ScoreManager.set_victory_condition("pieces", ScoreManager.total_pieces * 0.5)
